aa250332802e9c1ed4589ae9778f263b2ccb133f
apnorton <hskrus@gmail.com>
2015-11-30 14:05:32 -0500

    Minor tweaks to Preproc, installed scipy

--- code/Preproc.py
+++ code/Preproc.py
@@ -1,3 +1,11 @@
+import os
+import sys
+import numpy as np
+from sklearn.svm import SVC
+
 ##
 # This is a placeholder file; eventually, this will perform the preprocessing step
 ##
+
+if __name__ == '__main__':
+  print "I'm alive!"
